4fce21775393ece52918b2785aa6c25066a7f205
khadeejasherif <khadeejasherif372@gmail.com>
2026-07-21 00:19:20 +0300

    Update token header format in is-auth interceptor
    Remove 'Bearer' prefix from token in headers.

--- src/app/interceptors/is-auth.ts
+++ src/app/interceptors/is-auth.ts
@@ -6,7 +6,7 @@ export const isAuthInterceptor: HttpInterceptorFn = (req, next) => {
   if (token) {
     const newReq = req.clone({
       setHeaders: {
-        token: `Bearer ${token}`
+        token: ` ${token}`
       }
     });
     return next(newReq);
