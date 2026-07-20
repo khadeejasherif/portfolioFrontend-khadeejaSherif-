9fd6ba785cdf1af272c96b892c106adae07cf3db
khadeejasherif <khadeejasherif372@gmail.com>
2026-07-20 23:57:11 +0300

    Update is-auth.ts

--- src/app/interceptors/is-auth.ts
+++ src/app/interceptors/is-auth.ts
@@ -1,3 +1,19 @@
+// import { HttpInterceptorFn } from '@angular/common/http';
+
+// export const isAuthInterceptor: HttpInterceptorFn = (req, next) => {
+//   const token = localStorage.getItem('isAuth');
+
+//   if (token) {
+//     const newReq = req.clone({
+//       setHeaders: {
+//         token: `Bearer ${token}`
+//       }
+//     });
+//     return next(newReq);
+//   }
+
+//   return next(req);
+// };
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const isAuthInterceptor: HttpInterceptorFn = (req, next) => {
@@ -6,7 +22,7 @@ export const isAuthInterceptor: HttpInterceptorFn = (req, next) => {
   if (token) {
     const newReq = req.clone({
       setHeaders: {
-        token: `Bearer ${token}`
+        Authorization: `Bearer ${token}` // Changed from 'token: token' to standard Bearer format
       }
     });
     return next(newReq);
