dfc544ec2133d13dd9e364fadacecad6ea49eedb
khadeejasherif <khadeejasherif372@gmail.com>
2026-07-21 00:20:33 +0300

    Fix token header formatting in is-auth interceptor

--- src/app/interceptors/is-auth.ts
+++ src/app/interceptors/is-auth.ts
@@ -6,7 +6,7 @@ export const isAuthInterceptor: HttpInterceptorFn = (req, next) => {
   if (token) {
     const newReq = req.clone({
       setHeaders: {
-        token: ` ${token}`
+        token: token
       }
     });
     return next(newReq);
