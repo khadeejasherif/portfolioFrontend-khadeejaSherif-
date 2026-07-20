7aa53da3b333dd9f7d3bdec9735e3bef078d5f7f
khadeejasherif <khadeejasherif372@gmail.com>
2026-07-21 00:10:18 +0300

    Refactor token storage in login component
    Simplified token storage logic in user login.

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -43,11 +43,7 @@ export class Login {
 
     this.UserService.userLogin(this.loginForm.value).subscribe({
       next: (res: any) => {
-        console.log("Raw login response object:", res); // Check the console: is it res.token or something else?
-  
-  const token = res.token || res.accessToken;
-  localStorage.setItem('isAuth', token);
-  console.log("Stored token value:", localStorage.getItem('isAuth'));
+       localStorage.setItem('isAuth', res.token);
         
         this.UserService.getProfile().subscribe({
           next: (profileRes: any) => {
